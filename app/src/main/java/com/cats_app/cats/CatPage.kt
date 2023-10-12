01c60c96f766b4831ee53898f0dc2f33e3b0b2bd
Aqeel-Mohammed <just-dev+Aqeel-Mohammed@tyro.com>
2023-10-13 10:54:04 +1100

    add CatPage to show default cat list

--- app/src/main/java/com/cats_app/cats/CatPage.kt
+++ app/src/main/java/com/cats_app/cats/CatPage.kt
@@ -1,9 +1,62 @@
 package com.cats_app.cats
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.cats_app.domain.data.Cat
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CatPage() {
-
+fun CatPage(catsList: List<Cat>) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = "Cats",
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = Color(103, 58, 183, 255)
+                )
+            )
+        },
+        content = { padding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+            ) {
+                LazyColumn {
+                    items(catsList) { cat ->
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            text = cat.name,
+                            style = MaterialTheme.typography.titleLarge,
+                            color = Color(98, 20, 236, 255)
+                        )
+                    }
+                }
+            }
+        }
+    )
 
 }
